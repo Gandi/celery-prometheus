bdb0421f9dd88bb05a88a087bbbb28ae00803488
Michael Vieira <michael.vieira@gandi.net>
2023-03-03 20:07:33 +0100

    Add py.typed and make mypy happy

--- src/celery_prometheus/prometheus_bootstep.py
+++ src/celery_prometheus/prometheus_bootstep.py
@@ -1,37 +1,32 @@
 """Helper for celery."""
 import logging
 
-from celery import VERSION as celery_version
+from celery import VERSION as celery_version  # type: ignore
 from celery import Celery
-from prometheus_client import REGISTRY, start_http_server
-from prometheus_client.multiprocess import MultiProcessCollector
+from prometheus_client import REGISTRY, start_http_server  # type: ignore
+from prometheus_client.multiprocess import MultiProcessCollector  # type: ignore
 
 log = logging.getLogger(__name__)
 
 
 if celery_version.major < 5:
-
-    from celery.signals import user_preload_options
+    from celery.signals import user_preload_options  # type: ignore
 
     @user_preload_options.connect
-    def on_preload_parsed(options, **kwargs):
+    def on_preload_parsed(options, **kwargs) -> None:
         prometheus_collector_addr = options.get("prometheus_collector_addr")
         app = kwargs["app"]
         attach_prometheus_registry(app, prometheus_collector_addr)
 
 
-def add_prometheus_option(app):
-
-    help = "Celery Prometheus Configureation."
+def add_prometheus_option(app) -> None:
+    help = "Celery Prometheus Configuration."
     if celery_version.major < 5:
-
         def add_preload_arguments(parser):
             parser.add_argument("--prometheus-collector-addr", default=None, help=help)
 
         app.user_options["preload"].add(add_preload_arguments)
-
     else:
-
         from celery import bootsteps
         from click import Option
 
@@ -51,7 +46,6 @@ def attach_prometheus_registry(app: Celery, prometheus_addr: str) -> None:
 
     if prometheus_addr:
         MultiProcessCollector(REGISTRY)
-        ip, port = prometheus_addr.rsplit(":")
-        ip = int(ip)
-        port = int(port)
-        start_http_server(port, ip, REGISTRY)
+        prom_addr, prom_port = prometheus_addr.rsplit(":")
+        port = int(prom_port)
+        start_http_server(port, prom_addr, REGISTRY)

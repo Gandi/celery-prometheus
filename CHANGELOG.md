ef04e1ac7d2cb81e63cc0103ca473175a40ef6a9
Guillaume Gauvrit <guillaume.gauvrit@gandi.net>
2023-12-20 16:28:20 +0100

    Release 1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,12 @@
-## Release 1.0.1 (2023-05-22)
+## Release 1.1.0 - Released on 2023-12-20
+
+- Add a environment variable CELERY_PROMETHEUS_COLLECTOR_ADDR to configure the collector address.
+  
+
+## Release 1.0.1 - Released on 2023-05-22
 
 - Fix usage if the default registry REGISTRY was used
 
-## Release 1.0.0 (2023-03-0822)
+## Release 1.0.0 - Released on 2023-03-0822
 
 - Add typing support
--- scripts/write_changelog.py
+++ scripts/write_changelog.py
@@ -4,10 +4,10 @@ import datetime
 import celery_prometheus
 
 header = (
-    f"## {celery_prometheus.__version__} - "
+    f"## Release {celery_prometheus.__version__} - "
     f"Released on {datetime.datetime.now().date().isoformat()}"
 )
 with open("CHANGELOG.md.new", "w") as changelog:
     changelog.write(header)
     changelog.write("\n")
-    changelog.write("* please write here \n")
+    changelog.write("- please write here \n\n")
